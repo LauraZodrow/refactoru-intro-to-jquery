052f1e40f6fd3001e9fe11a2ba83c952b5ef2b42
Laura <laura.zodrow@gmail.com>
2014-09-17 14:05:00 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,48 @@
+// $(document).on('ready', function() {
+//   $('.button-click').click(function(){
+//   	// console.log("Hello! its a button");
+//   	var sayOk = confirm("Do you want to navigate to this URL?")
+// 	  	if (sayOk === true) {
+// 				$('body').append("It's at button!<br>");
+// 				return true
+// 			} else {
+// 				$('.button-click').css("display", "none");
+// 				return false
+// 			}
+//   });
+
+//   $('.button-alt').click(function(){
+// 	var sayOk = confirm("Do you want to navigate to this URL?")
+// 		if (sayOk === true) {
+// 			$('h1').append('You clicked!<br>');
+// 			return true
+// 		} else {
+// 			$('.button-alt').css("display", "none");
+// 			return false
+// 		}
+//   });
+
+//   $('.button-three').click(function(){
+//     var sayOk = confirm("Do you want to navigate to this URL?")
+// 	  	if (sayOk === true) {
+// 				$('ul').append('<li>three items</li>');
+// 				return true
+// 			} else {
+// 				$('.button-three').css("display", "none");
+// 				return false
+// 			}
+//   });
+
+//   $('p').hover(function() {
+//   	$('p').css("color", "pink");
+//   	}, function() {
+//   	$('p').css("color", "black");
+//   });
+//   $('h2').append("!");
+
+// });
+
+$(document).on('ready', function() {
+	
+
+});
